7fcfcf0a8b49d21f455cc037af4a570a5f61c754
irygiels <tojestpozadanylogin@gmail.com>
2017-05-16 14:22:47 +0200

    notes added

--- numpy_.py
+++ numpy_.py
@@ -14,7 +14,12 @@ o = np.linspace(0, 4, 9) #from 0 to 4: 9 values
 o.resize(3,3) #resize changes array's shape; reshape only outputs temp array reshape
 print(o)
 
-a = np.array([1, 2, 3] * 3) #appends next array
+a = np.array([1, 2, 3] * 3) #appends next array(s)
 b = np.repeat([1, 2, 3], 3) #repeats every element
 c = np.vstack([a,b]) #hstack can be used to append horizontically
 print(c)
+
+z = np.array([y, y**2])
+z = z.T #transpose
+z = z.astype('f') #z.dtype will return float32 instead of int64
+#operations posible on an array: sum(), max(), min(), mean(), std(); indexes: argmax(), argmin()
