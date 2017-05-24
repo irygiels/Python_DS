29caac00244577769aa6724eb35c8e3b56018f35
irygiels <tojestpozadanylogin@gmail.com>
2017-05-24 18:47:24 +0200

    simple sniffer for IPv4 packets

---
+++ protocols_statistics.py
@@ -0,0 +1,44 @@
+import socket, sys, time
+from struct import *
+
+def minute_passed(oldepoch):
+    return time.time() - oldepoch >= 60
+
+def eth_addr (a) :
+  b = "%.2x:%.2x:%.2x:%.2x:%.2x:%.2x" % (ord(a[0]) , ord(a[1]) , ord(a[2]), ord(a[3]), ord(a[4]) , ord(a[5]))
+  return b
+ 
+try:
+    s = socket.socket( socket.AF_PACKET , socket.SOCK_RAW , socket.ntohs(0x0003))
+except socket.error , msg:
+    print 'Socket could not be created. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+    sys.exit()
+
+
+counters = {"eth": 0, "ip": 0, "icmp_ip": 0, "tcp_ip": 0, "udp_ip": 0, "diff_ip": 0}
+i=0
+oldtime=time.time()
+while True:
+    packet = s.recvfrom(65565)
+    packet = packet[0]
+    counters['eth']+=1
+    eth_length = 14 
+    eth_header = packet[:eth_length]
+    eth = unpack('!6s6sH' , eth_header)
+    eth_protocol = socket.ntohs(eth[2])
+    #IP = 8
+    i+=1
+    if eth_protocol == 8 :
+	counters['ip']+=1
+        ip_header = packet[eth_length:20+eth_length]
+        iph = unpack('!BBHHHBBH4s4s' , ip_header)
+        protocol = iph[6]
+        if protocol == 1 :
+            counters['icmp_ip']+=1
+        elif protocol == 6 :
+            counters['tcp_ip']+=1
+        elif protocol == 17 :
+            counters['udp_ip']+=1
+    if(time.time() - oldtime > 3) :
+        print 'All packets {}: IP packets {} incl. ICMP {}, TCP {}, UDP {}'.format(counters['eth'], counters['ip'], counters['icmp_ip'], counters['tcp_ip'], counters['udp_ip'])
+        oldtime = time.time()
